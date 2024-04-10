e4b9521c108f5ffaeed1bdb0bc139a3bc9ce4850
turtel42 <papadimitrios216@gmail.com>
2024-04-10 18:02:34 +0300

    added SpringProject files

---
+++ MyUoWM-Admin-Panel/src/test/java/gr/ieee/cs/uowm/myuowm_admin_panel/MyUoWmAdminPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.ieee.cs.uowm.myuowm_admin_panel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUoWmAdminPanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
