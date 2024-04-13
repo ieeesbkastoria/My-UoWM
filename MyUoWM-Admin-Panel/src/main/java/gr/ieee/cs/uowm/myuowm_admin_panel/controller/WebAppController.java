f8f8e383c97766a7f34b5a9ad01e62e89feadd22
turtel42 <papadimitrios216@gmail.com>
2024-04-13 21:57:46 +0300

    Fixed GetMapping Url

--- MyUoWM-Admin-Panel/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/controller/WebAppController.java
+++ MyUoWM-Admin-Panel/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/controller/WebAppController.java
@@ -8,48 +8,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import java.sql.Time;
 import java.util.List;
 
 @RestController
-@RequestMapping("/myuowm")
+@RequestMapping("/")
 public class WebAppController {
 
 
     //Serve Static Files
-    @GetMapping("/timetable")
+    @GetMapping("/myuowm/timetable")
     public TimeTable getTimeTable() {
         //TODO
         return new TimeTable("/");
     }
 
-    @GetMapping("/lesxi")
+    @GetMapping("/myuowm/lesxi")
     public DinnerPlan getMealPlan() {
         //TODO
         return new DinnerPlan("/");
     }
 
-    @GetMapping("/personal")
+    @GetMapping("/myuowm/personal")
     public List<Personal> getAllPersonalInfo() {
         //TODO
         return null;
     }
 
-    @GetMapping("/personl{personalId}")
+    @GetMapping("/myuowm/personal{personalId}")
     public Personal getPersonal(@PathVariable("personalId") String personalID) {
         //TODO
         return null;
     }
 
-    @GetMapping("/clubs")
+    @GetMapping("/myuowm/clubs")
     public List<Club> getAllClubInfo() {
         //TODO
         return null;
     }
 
-    @GetMapping("/clubs{clubId}")
+    @GetMapping("/myuowm/clubs{clubId}")
     public Club getClubDetail(@PathVariable("clubId") String clubID) {
         //TODO
         return null;
