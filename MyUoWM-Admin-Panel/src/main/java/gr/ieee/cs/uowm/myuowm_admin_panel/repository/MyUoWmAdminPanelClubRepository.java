b0132751c92959745a395b26de4ba3a6909d49e0
Soti <sotosz04@yahoo.gr>
2024-04-15 14:04:45 +0300

    Added repository layer

---
+++ MyUoWM-Admin-Panel/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/repository/MyUoWmAdminPanelClubRepository.java
@@ -0,0 +1,8 @@
+package gr.ieee.cs.uowm.myuowm_admin_panel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import gr.ieee.cs.uowm.myuowm_admin_panel.model.Club;
+
+public interface MyUoWmAdminPanelClubRepository extends JpaRepository<Club, String> {
+
+}
