f2a8b02bee1e8b2b78e516e86789c2e7107b3d21
turtel42 <papadimitrios216@gmail.com>
2024-04-22 21:11:28 +0300

    added Clubs functionality

--- MyUoWM-Admin-Panel/Client/myuowm-admin-panel-ui/src/App.js
+++ MyUoWM-Admin-Panel/Client/myuowm-admin-panel-ui/src/App.js
@@ -1,11 +1,12 @@
 import Personnel from './pages/Personnel';
+import Clubs from "./pages/Clubs";
 
 function App() {
   return (
     <div className="App">
       <p>Hello myuowm</p>
       
-      <Personnel />
+      <Clubs />
     </div>
   );
 }
