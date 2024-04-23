63d79f67fb77234ab7f5d635d0a4807e8fb81a62
turtel42 <papadimitrios216@gmail.com>
2024-04-23 16:19:50 +0300

    added auth service and controller

---
+++ MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/auth/AuthenticationRequest.java
@@ -0,0 +1,7 @@
+package gr.ieee.cs.uowm.myuowm_admin_panel.auth;
+
+public class AuthenticationRequest {
+
+    private String email;
+    String password;
+}
--- MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/config/JwtAuthenticationFilter.java
+++ MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/config/JwtAuthenticationFilter.java
@@ -1,13 +1,25 @@
 package gr.ieee.cs.uowm.myuowm_admin_panel.config;
 
 import gr.ieee.cs.uowm.myuowm_admin_panel.token.TokenRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
