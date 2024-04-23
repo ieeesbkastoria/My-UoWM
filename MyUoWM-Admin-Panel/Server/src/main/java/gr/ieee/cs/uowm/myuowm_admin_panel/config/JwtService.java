446ae6884f41ab3842670ae598371082380ae846
turtel42 <papadimitrios216@gmail.com>
2024-04-23 16:51:05 +0300

    fixed config errors

--- MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/config/JwtService.java
+++ MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/config/JwtService.java
@@ -1,12 +1,19 @@
 package gr.ieee.cs.uowm.myuowm_admin_panel.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
--- MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/config/SecurityConfiguration.java
+++ MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/config/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
 import static gr.ieee.cs.uowm.myuowm_admin_panel.user.Permission.*;
 import static gr.ieee.cs.uowm.myuowm_admin_panel.user.Role.ADMIN;
