59bf3c586c1320ad81777d058f6f1321b609342d
turtel42 <papadimitrios216@gmail.com>
2024-04-24 01:26:52 +0300

    fixed security config typo

--- MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/config/SecurityConfiguration.java
+++ MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/config/SecurityConfiguration.java
@@ -25,7 +25,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
-    private static final String[] WHITE_LIST_URL = {"/api/auth" , "/myuowm/**"}; //TODO
+    private static final String[] WHITE_LIST_URL = {"/api/auth/**" , "/myuowm/**"}; //TODO
 
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
