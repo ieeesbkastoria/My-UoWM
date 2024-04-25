8dac80bf9a57fcf6e0ce6754640281c936644853
turtel42 <papadimitrios216@gmail.com>
2024-04-25 15:37:53 +0300

    updated UpdateLinks

--- MyUoWM-Admin-Panel/Client/myuowm-admin-panel-ui/src/pages/UpdateLinks.js
+++ MyUoWM-Admin-Panel/Client/myuowm-admin-panel-ui/src/pages/UpdateLinks.js
@@ -5,12 +5,32 @@ const UpdateLinks = () => {
 
   const [links, error, isPending] = useFetch('http//localhost:8080/api/myuowm/links');
 
-  const handleClick = (e) => {}
+  const handleClick = (e) => {
+    //TODO implement put request to server
+  }
 
   return (
     <div className="links-update">
       {isPending && <div>Loading...</div>}
       {error && <div>{ error }</div>}
+      <form>
+        <label>Lesxi</label>
+        <input
+          type="url"
+          value={ links.lesxi }
+          required
+          onChange={ (e) => (links.lesxi = e.target.value)}
+        />
+        <label>Eudoksos</label>
+        <input
+          type="url"
+          value={ links.eudoksos }
+          required
+          onChange={ (e) => (links.eudoksos = e.target.value)}
+        />
+        {/* TODO add other links */}
+        <button onClick={ () => handleClick(links) }>Submit</button>
+      </form>
         
     </div>
   )
