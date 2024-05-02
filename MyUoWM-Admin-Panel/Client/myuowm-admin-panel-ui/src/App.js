5615e42b7b2d462ebfda158893aefafd7f286a88
Turtel216 <papadimitrios216@gmail.com>
2024-05-02 22:22:04 +0300

    added Routing

--- MyUoWM-Admin-Panel/Client/myuowm-admin-panel-ui/src/App.js
+++ MyUoWM-Admin-Panel/Client/myuowm-admin-panel-ui/src/App.js
@@ -1,12 +1,11 @@
 import Login from './pages/Login';
-
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello myuowm</p>
-      <Login />
-    </div>
+    <Routes>
+      <Route path="login" element={<Login />} />
+    </Routes>
   );
 }
 
