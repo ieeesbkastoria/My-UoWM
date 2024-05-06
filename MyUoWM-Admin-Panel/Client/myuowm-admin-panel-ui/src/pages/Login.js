cc46ec47eb6adae9a9a2282aaf8932a0ec9f548d
Turtel216 <papadimitrios216@gmail.com>
2024-05-06 19:11:50 +0300

    updated client auth endpoints

--- MyUoWM-Admin-Panel/Client/myuowm-admin-panel-ui/src/pages/Login.js
+++ MyUoWM-Admin-Panel/Client/myuowm-admin-panel-ui/src/pages/Login.js
@@ -3,7 +3,7 @@ import useAuth from '../hooks/useAuth';
 import { Link, useNavigate, useLocation } from "react-router-dom";
 
 import axios from '../api/axios';
-const LOGIN_URL = '/api/auth';
+const LOGIN_URL = '/api/auth/login';
 
 const Login = () => {
   const { setAuth } = useAuth();
--- MyUoWM-Admin-Panel/Client/myuowm-admin-panel-ui/src/pages/SignUp.js
+++ MyUoWM-Admin-Panel/Client/myuowm-admin-panel-ui/src/pages/SignUp.js
@@ -5,7 +5,7 @@ import axios from '../api/axios';
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-const REGISTER_URL = '/register';
+const REGISTER_URL = '/api/auth/register';
 
 const SignUp = () => {
   const userRef = useRef();
