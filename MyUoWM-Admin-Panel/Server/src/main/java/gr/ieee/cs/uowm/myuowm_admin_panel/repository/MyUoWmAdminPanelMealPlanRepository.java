ea5052ea789dacd46b55ba0d528522e9aff4ad0d
Turtel216 <papadimitrios216@gmail.com>
2024-05-06 20:41:32 +0300

    added MealPlan repository interface

---
+++ MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/repository/MyUoWmAdminPanelMealPlanRepository.java
@@ -0,0 +1,7 @@
+package gr.ieee.cs.uowm.myuowm_admin_panel.repository;
+
+import gr.ieee.cs.uowm.myuowm_admin_panel.model.MealPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MyUoWmAdminPanelMealPlanRepository extends JpaRepository<MealPlan, Long> {
+}
