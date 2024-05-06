c8a18380150831181b94a4db731fa87291e6af69
Turtel216 <papadimitrios216@gmail.com>
2024-05-06 20:38:44 +0300

    added MealPlan model

---
+++ MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/model/MealPlan.java
@@ -0,0 +1,33 @@
+package gr.ieee.cs.uowm.myuowm_admin_panel.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "meal_plan")
+@Getter
+@Setter
+public class MealPlan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private int week_number;
+    @Column(nullable = false)
+    private String day_of_week;
+    @Column(nullable = false)
+    private String dish1;
+    // TODO change String of meal type to sql enum(Breakfast, lunch, dinner)
+    @Column(nullable = false)
+    private String meal_type;
+    @Column(nullable = false)
+    private String dish2;
+    @Column(nullable = false)
+    private String salad;
+    @Column(nullable = false)
+    private String dessert;
+
+
+}
