bf49c6552ebf448e134fdc1e9e7318cbb72e6c69
Turtel216 <papadimitrios216@gmail.com>
2024-05-07 22:39:25 +0300

    Added MealPlan page

---
+++ MyUoWM-Admin-Panel/Client/src/pages/MealPlan.js
@@ -0,0 +1,18 @@
+import WeekTable from '../components/WeekTable'
+
+const MealPlan = () => {
+
+  return (
+    <div ClassName="meal-plan">
+      <h1>Create/Update meal plan</h1>
+    
+      <h2>Week 1</h2>
+
+      <h2>Week 2</h2>
+      
+      <h2>Week 3</h2>
+     
+      <h2>Week 4</h2>
+    </div>
+  )
+};
