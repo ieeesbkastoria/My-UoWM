bcafb1c130c518ac65a37c0e3a5daeeb84afc301
Turtel216 <papadimitrios216@gmail.com>
2024-05-07 22:48:06 +0300

    Updated MealPlan

--- MyUoWM-Admin-Panel/Client/src/pages/MealPlan.js
+++ MyUoWM-Admin-Panel/Client/src/pages/MealPlan.js
@@ -7,11 +7,11 @@ const MealPlan = () => {
       <h1>Create/Update meal plan</h1>
     
       <h2>Week 1</h2>
-
+      <WeekTable />
       <h2>Week 2</h2>
-      
+      <WeekTable />
       <h2>Week 3</h2>
-     
+      <WeekTable />
       <h2>Week 4</h2>
     </div>
   )
