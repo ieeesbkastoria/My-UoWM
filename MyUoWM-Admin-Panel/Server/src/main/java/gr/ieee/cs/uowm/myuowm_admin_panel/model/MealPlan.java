f38dd865a981e049948b85cad31614b6f8e015d2
Turtel216 <papadimitrios216@gmail.com>
2024-05-07 21:39:09 +0300

    Updated MealPlan model

--- MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/model/MealPlan.java
+++ MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/model/MealPlan.java
@@ -1,37 +1,32 @@
 package gr.ieee.cs.uowm.myuowm_admin_panel.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "meal_plan")
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 public class MealPlan {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Byte id;
+    @Column(nullable = false)
+    private Long meal_id;
     @Column(nullable = false)
     private int week_number;
     @Column(nullable = false)
     private String day_of_week;
     @Column(nullable = false)
     private String dish1;
-    // TODO change String of meal type to sql enum(Breakfast, lunch, dinner)
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private String meal_type;
+    private MealType mealType;
     @Column(nullable = false)
     private String dish2;
     @Column(nullable = false)
     private String salad;
     @Column(nullable = false)
     private String dessert;
-
-
 }
