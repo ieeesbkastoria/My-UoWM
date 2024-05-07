ba07c56f01434906c158819f250450e6411a1014
Turtel216 <papadimitrios216@gmail.com>
2024-05-07 22:46:18 +0300

    Update WeekTable

--- MyUoWM-Admin-Panel/Client/src/components/WeekTable.js
+++ MyUoWM-Admin-Panel/Client/src/components/WeekTable.js
@@ -1,10 +1,18 @@
+import { useState } from 'react'
 
 const WeekTable = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
     <div classNane="week-table">
-
+      <button onClick={ handleOpen }>Edit</button>
+      { open ? <div>Is Open</div> : <div></div>}
     </div>
-  )
+  );
 };
 
 export default WeekTable;
