37631bcceed0309ab1e1989f7436a35a8d56255a
Turtel216 <papadimitrios216@gmail.com>
2024-05-12 02:00:08 +0300

    Updated datasource layer

--- MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/datasource/repository/MyUoWmAdminPanelMealPlanRepository.java
+++ MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/datasource/repository/MyUoWmAdminPanelMealPlanRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface MyUoWmAdminPanelMealPlanRepository extends JpaRepository<MealPlan, Long> {
-    public List<MealPlan> findByMealId(Byte id);
+    List<MealPlan> findByMealId(Byte id);
 }
