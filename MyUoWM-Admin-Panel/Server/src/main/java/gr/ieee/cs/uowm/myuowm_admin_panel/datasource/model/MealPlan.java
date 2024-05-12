d17c7ccd08be84a446e796fc2ab0aae5e761b438
Turtel216 <papadimitrios216@gmail.com>
2024-05-12 12:14:20 +0300

    Added MyUoWmAdminPanelMealPlanRepositoryTest

--- MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/datasource/model/MealPlan.java
+++ MyUoWM-Admin-Panel/Server/src/main/java/gr/ieee/cs/uowm/myuowm_admin_panel/datasource/model/MealPlan.java
@@ -1,11 +1,15 @@
 package gr.ieee.cs.uowm.myuowm_admin_panel.datasource.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "meal_plan")
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class MealPlan {
@@ -16,16 +20,16 @@ public class MealPlan {
     @Column(nullable = false)
     private Long mealId;
     @Column(nullable = false)
-    private int week_number;
+    private Integer week_number;
     @Column(nullable = false)
     private String day_of_week;
     @Column(nullable = false)
     private String dish1;
+    @Column(nullable = false)
+    private String dish2;
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private MealType mealType;
-    @Column(nullable = false)
-    private String dish2;
     @Column(nullable = false)
     private String salad;
     @Column(nullable = false)
