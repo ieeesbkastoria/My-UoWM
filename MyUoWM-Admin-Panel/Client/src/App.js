9306a0f84b9fe9299d33f99a8f66109e32ee50d5
turtel42 <papadimitrios216@gmail.com>
2024-05-15 01:25:29 +0300

    Updated Node dependencies

--- MyUoWM-Admin-Panel/Client/src/App.js
+++ MyUoWM-Admin-Panel/Client/src/App.js
@@ -1,10 +1,12 @@
 import Login from './pages/Login';
+import SignUp from './pages/SignUp'
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Routes>
       <Route path="login" element={<Login />} />
+      <Route path="register" element={<SignUp />} />
     </Routes>
   );
 }
--- MyUoWM-Admin-Panel/Client/src/pages/SignUp.js
+++ MyUoWM-Admin-Panel/Client/src/pages/SignUp.js
@@ -2,6 +2,7 @@ import { useRef, useState, useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from '../api/axios';
+import { Link } from "react-router-dom";
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
@@ -172,7 +173,7 @@ const SignUp = () => {
                     <p>
                         Already registered?<br />
                         <span className="line">
-                            <Link to="/">Sign In</Link>
+                            <Link to="login">Sign In</Link>
                         </span>
                     </p>
                 </section>
