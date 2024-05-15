2e3421a2f6ee3184beb277f72885d5392a2048a7
turtel42 <papadimitrios216@gmail.com>
2024-05-15 10:42:44 +0300

    Updated useAuth

--- MyUoWM-Admin-Panel/Client/src/hooks/useAuth.js
+++ MyUoWM-Admin-Panel/Client/src/hooks/useAuth.js
@@ -1,8 +1,10 @@
-import { useContext } from "react";
-import AuthContext from "../components/AuthProvider";
+import { useContext, useDebugValue } from "react";
+import AuthContext from '../components/AuthProvider';
 
 const useAuth = () => {
-  return useContext(AuthContext);
+    const { auth } = useContext(AuthContext);
+    useDebugValue(auth, auth => auth?.user ? "Logged In" : "Logged Out")
+    return useContext(AuthContext);
 }
 
 export default useAuth;
