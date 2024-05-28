0cf7768592b1b898ae0e6f29de935e16cd95a0e4
Turtel216 <papadimitrios216@gmail.com>
2024-05-29 02:05:57 +0300

    Updated Home Page

--- MyUoWM-Admin-Panel/Client/src/pages/Home.js
+++ MyUoWM-Admin-Panel/Client/src/pages/Home.js
@@ -15,12 +15,14 @@ const Home = () => {
   return (
   <>
     <Nav />
-    <div>
-        <p>The user-friendly CMS Edit Suite empowers authorized university personnel to manage key website aspects. Effortlessly update hyperlinks, maintain a dynamic cafeteria meal plan, and keep faculty and staff profiles current – all through an intuitive interface, improving website accuracy, user experience, and communication efficiency.
-</p>
+    <div className="my-1 flex justify-center items-center h-screen">
+      <div className="bg-slate-800 p-8 rounded shadow border border-gray-200 max-w-md">
+          <p className="text-gray-50">The user-friendly CMS Edit Suite empowers authorized university personnel to manage key website aspects. Effortlessly update hyperlinks, maintain a dynamic cafeteria meal plan, and keep faculty and staff profiles current – all through an intuitive interface, improving website accuracy, user experience, and communication efficiency.</p>
+      </div>
     </div>
   </>
-)
+);
+
 }
 
 
