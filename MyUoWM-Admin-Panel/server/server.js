9f5311e6d994cb91120a05fd116bb5426c1de888
Turtel216 <papadimitrios216@gmail.com>
2024-11-19 20:41:53 +0200

    Fixed .env typo

--- MyUoWM-Admin-Panel/server/server.js
+++ MyUoWM-Admin-Panel/server/server.js
@@ -21,7 +21,7 @@ app.use('/api/links', LinkRoutes)
 app.use('/api/user', userRoutes)
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
