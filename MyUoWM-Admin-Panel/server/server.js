4c5d16f698582fbbb90e24b792ae2a17d6e0c448
Turtel216 <papadimitrios216@gmail.com>
2024-11-19 21:36:48 +0200

    Fixed port numbers

--- MyUoWM-Admin-Panel/server/server.js
+++ MyUoWM-Admin-Panel/server/server.js
@@ -11,11 +11,6 @@ const app = express()
 // middleware
 app.use(express.json())
 
-app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
-
 // routes
 app.use('/api/links', LinkRoutes)
 app.use('/api/user', userRoutes)
