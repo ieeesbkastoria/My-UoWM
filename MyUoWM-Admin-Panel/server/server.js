81acee608aa3a5badedd7023fc419d3c10f00d78
Turtel216 <papadimitrios216@gmail.com>
2024-11-20 00:36:43 +0200

    Updated server logging

--- MyUoWM-Admin-Panel/server/server.js
+++ MyUoWM-Admin-Panel/server/server.js
@@ -5,32 +5,34 @@ const mongoose = require('mongoose')
 const linkRoutes = require('./routes/links')
 const userRoutes = require('./routes/user')
 
+const cors = require('cors');
+
 // express app
 const app = express()
 
 // middleware
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
+app.use(cors());
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
-require('http').maxHeaderSize = 16384; // Increase to 16KB
-
+// Log request headers to understand what's large
+app.use((req, res, next) => {
+  console.log('Request Headers:', req.headers);
+  console.log('Cookie Length:', req.headers.cookie?.length);
+  next();
+});
 
 // routes
 app.use('/api/links', linkRoutes)
 app.use('/api/user', userRoutes)
 
-app.use((req, res, next) => {
-  console.log('Headers:', req.headers);
-  next()
-})
-
-
-
 // connect to db
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
+    const server = app.listen(process.env.PORT, () => {
+      server.maxHeadersCount = 2000; // Increase max headers
+      server.headersTimeout = 60 * 1000; // Increase headers timeout
       console.log('connected to db & listening on port', process.env.PORT)
     })
   })
