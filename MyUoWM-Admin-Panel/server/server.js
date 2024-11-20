0772a0102a331a12d22ecd874ae9be2ddb762b00
Turtel216 <papadimitrios216@gmail.com>
2024-11-20 17:08:31 +0200

    Removed unnecessary Express configs

--- MyUoWM-Admin-Panel/server/server.js
+++ MyUoWM-Admin-Panel/server/server.js
@@ -5,22 +5,11 @@ const mongoose = require('mongoose')
 const linkRoutes = require('./routes/links')
 const userRoutes = require('./routes/user')
 
-const cors = require('cors');
-
 // express app
 const app = express()
 
 // middleware
-app.use(cors());
-app.use(express.json({ limit: '500mb' }));
-app.use(express.urlencoded({ limit: '500mb', extended: true }));
-
-// Log request headers to understand what's large
-app.use((req, res, next) => {
-  console.log('Request Headers:', req.headers);
-  console.log('Cookie Length:', req.headers.cookie?.length);
-  next();
-});
+app.use(express.json());
 
 // routes
 app.use('/api/links', linkRoutes)
@@ -30,9 +19,7 @@ app.use('/api/user', userRoutes)
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     // listen for requests
-    const server = app.listen(process.env.PORT, () => {
-      server.maxHeadersCount = 2000; // Increase max headers
-      server.headersTimeout = 60 * 1000; // Increase headers timeout
+    app.listen(process.env.PORT, () => {
       console.log('connected to db & listening on port', process.env.PORT)
     })
   })
